19996b61801b91d197d655f5be58e37737793d2c
Ashutosh <pashutosh563@email.com>
2025-12-09 16:16:25 +0530

    SS

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const AuthController = require('../controllers/AuthController');
 const UserController = require('../controllers/UserController');
 
-router.post('/signup', authController.signup);
-router.post('/signin', authController.signin);
+router.post('/signup', AuthController.signup);
+router.post('/signin', AuthController.signin);
 router.get('/user', UserController.getUserDetails);
 
 
